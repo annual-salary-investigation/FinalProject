3c63b190b90ff445043db35ae7d16a23cdc0d863
Gayeon-Leee <123913974+Gayeon-Leee@users.noreply.github.com>
2023-07-07 00:24:14 +0900

    차량 관리 화면 DB 연동

--- MonitoringApp_mng/가연/appTemplate/Views/MngCar.xaml.cs
+++ MonitoringApp_mng/가연/appTemplate/Views/MngCar.xaml.cs
@@ -1,6 +1,12 @@
-﻿using MahApps.Metro.Controls;
+﻿using appTemplate.Logics;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +29,96 @@ namespace appTemplate.Views
         public MngCar()
         {
             InitializeComponent();
+        }
+
+        private async void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            this.DataContext = null;
+
+            List<CarList> cars = new List<CarList>();
+            try 
+            {
+                using (MySqlConnection conn = new MySqlConnection(Commons.MyConnString)) 
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                    var query = $@"SELECT Id,
+                                         RoomNum,
+                                         CarNum,
+                                         PhoneNum,
+                                         SpecialNote
+                                    FROM miniproject.carmng";
+
+                    var cmd = new MySqlCommand(query, conn);
+                    var adapter = new MySqlDataAdapter(cmd);
+                    var dSet = new DataSet();
+                    adapter.Fill(dSet, "CarList");
+
+                    foreach (DataRow dr in dSet.Tables["CarList"].Rows)
+                    {
+                        cars.Add(new CarList
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            RoomNum = Convert.ToInt32(dr["RoomNum"]),
+                            CarNum = Convert.ToInt32(dr["CarNum"]),
+                            PhoneNum = Convert.ToString(dr["PhoneNum"]),
+                            SpecialNote = Convert.ToString(dr["SpecialNote"])
+                        });
+                    }
+                    Debug.WriteLine("테스트");
+                    Debug.WriteLine(cars[0]);
+
+                    this.DataContext = cars;
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"DB조회 오류 {ex.Message}", MessageDialogStyle.Affirmative, null);
+            }
+        }
+
+        private async void BtnCarlist_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = null;
+
+            List<CarList> cars = new List<CarList>();
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Commons.MyConnString))
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                    var query = $@"SELECT Id,
+                                         RoomNum,
+                                         CarNum,
+                                         PhoneNum,
+                                         SpecialNote
+                                    FROM miniproject.carmng";
+
+                    var cmd = new MySqlCommand(query, conn);
+                    var adapter = new MySqlDataAdapter(cmd);
+                    var dSet = new DataSet();
+                    adapter.Fill(dSet, "CarList");
+
+                    foreach (DataRow dr in dSet.Tables["CarList"].Rows)
+                    {
+                        cars.Add(new CarList
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            RoomNum = Convert.ToInt32(dr["RoomNum"]),
+                            CarNum = Convert.ToInt32(dr["CarNum"]),
+                            PhoneNum = Convert.ToString(dr["PhoneNum"]),
+                            SpecialNote = Convert.ToString(dr["SpecialNote"])
+                        });
+                    }
+                    this.DataContext = cars;
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"DB조회 오류 {ex.Message}", MessageDialogStyle.Affirmative, null);
+            }
         }
     }
 }
